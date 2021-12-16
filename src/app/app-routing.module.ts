55cb81d15e25a9b278c34d3243362437517c4957
Kunal <kunalkumaarsingh@gmail.com>
2021-12-16 11:38:50 +0530

    added dynamic div module

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,12 +3,12 @@ import { Routes, RouterModule, PreloadAllModules } from '@angular/router';
 import { AppComponent } from './app.component';
 
 const routes: Routes = [
-  { path: "", redirectTo: "/countdownTimerIO", pathMatch: "full" },
+  { path: "", redirectTo: "/dynamicDiv", pathMatch: "full" },
   { path: "animation", loadChildren: () => import("./animation/animation.module").then(m => m.AnimationModule) },
   { path: "eCommerce", loadChildren: () => import("./e-commerce/e-commerce.module").then(m => m.ECommerceModule) },
   { path: "countdownTimerIO", loadChildren: () => import("./countdown-timer/countdown-timer.module").then(m => m.CountdownTimerModule) },
   { path: "studentsMarksTable", loadChildren: () => import("./students-marks-table/students-marks-table.module").then(m=>m.StudentsMarksTableModule) },
-
+  { path: "dynamicDiv", loadChildren: () => import("./dynamic-div/dynamic-div.module").then(m=>m.DynamicDivModule) },
 ];
 
 @NgModule({
---
+++ src/app/dynamic-div/dynamic-div/dynamic-div.component.ts
@@ -0,0 +1,61 @@
+import { AfterViewChecked, AfterViewInit, Component, ElementRef, HostListener, OnDestroy, OnInit, Renderer2, ViewChild } from '@angular/core';
+
+@Component({
+  selector: 'app-dynamic-div',
+  templateUrl: './dynamic-div.component.html',
+  styleUrls: ['./dynamic-div.component.scss']
+})
+export class DynamicDivComponent implements OnInit, OnDestroy {
+  dynamicArray = new Array(3);
+  count = 0
+  clicklistener;
+  constructor(private renderer: Renderer2, private el: ElementRef) { }
+
+  ngOnInit() {
+    for (let i = 0; i < this.dynamicArray.length; i++) {
+      this.createDynamicDiv(i);
+    }
+  }
+
+
+  createDynamicDiv(index) {
+    const div = this.renderer.createElement('div');
+    const button = this.renderer.createElement('button');
+    const text = this.renderer.createText('Click Me');
+    this.renderer.setAttribute(button, 'aria-hidden', 'true');
+    this.clicklistener = this.renderer.listen(button, 'click', (evt) => {
+      this.generateAlert(index)
+     })
+    this.renderer.appendChild(button, text);
+    this.renderer.appendChild(div, button);
+    this.renderer.appendChild(this.el.nativeElement, div);
+  }
+
+  generateAlert(index) {
+    if (index == 0) {
+      alert(`Button in ${index + 1}st div clicked`)
+    } else if (index == 1) {
+      alert(`Button in ${index + 1}nd div clicked`)
+    } else if (index == 2) {
+      alert(`Button in ${index + 1}3d div clicked`)
+    } else {
+      alert(`Button in ${index + 1}th div clicked`)
+    }
+  }
+
+
+
+  ngOnDestroy() {
+    this.clicklistener.unsubscribe()
+  }
+
+  @HostListener("window:scroll", ['$event'])
+  scrollMe(event) {
+    this.count++
+    if (this.count == 5) {
+      this.dynamicArray.push("");
+      this.createDynamicDiv(this.dynamicArray.length);
+      this.count = 0;
+    }
+  }
+}
